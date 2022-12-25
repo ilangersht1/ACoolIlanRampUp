17d6dda12a4b2d1a46b84b42adb98a0274e4cee6
ilangersht1 <ilan.gersht@redis.com>
2022-12-25 17:11:27 +0200

    add more instances and EIP

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_network_interface" "ilan-network-interface" {
+  count = 2
   subnet_id   = aws_subnet.public.id
-  private_ips = ["10.0.1.100"]
   security_groups = [aws_security_group.sg-ilan.id]
 
   tags = {
@@ -13,14 +13,13 @@ resource "aws_instance" "ilan-instance" {
   ami           = "ami-0a261c0e5f51090b1"
   instance_type = "t2.micro"
   network_interface {
-    network_interface_id = aws_network_interface.ilan-network-interface.id
+    network_interface_id = aws_network_interface.ilan-network-interface[count.index].id
     device_index         = 0
   }
-
+  count = 2
   tags = {
     Owner  = "iLan"
     Delete = "True"
     Name   = "iLan-instance"
   }
-
 }
--- eip.tf
+++ eip.tf
@@ -1,8 +1,15 @@
 resource "aws_eip" "lb" {
-  instance = aws_instance.ilan-instance.id
   vpc      = true
   tags = {
     Owner  = "iLan"
     Delete = "True"
   }
+  count = 2
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  count = 2
+  allocation_id = aws_eip.lb[count.index].id
+  instance_id = aws_instance.ilan-instance[count.index].id
+
 }

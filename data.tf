b0ebc198ce8acd96b5ceb2bb97efb12b9ed708b8
ilangersht1 <120910800+ilangersht1@users.noreply.github.com>
2023-01-03 10:28:12 +0200

    Update data.tf
    Co-authored-by: Ben Kaplan <77549469+kaplanben@users.noreply.github.com>

--- data.tf
+++ data.tf
@@ -2,6 +2,6 @@ data "aws_ami" "rempup_ami_version" {
   owners = ["amazon"]
   filter {
     name   = "image-id"
-    values = ["ami-09c5ba4f838d8684a"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
   }
 }
